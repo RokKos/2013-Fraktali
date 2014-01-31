bb34c8b2c3d931d340ebf8e50cfea12c9ddc3b5a
Natan Žabkar <natan.zabkar@gmail.com>
2014-01-31 12:03:02 +0100

    pep8

--- numpy_pillow_example.py
+++ numpy_pillow_example.py
@@ -1,12 +1,20 @@
+# -*- coding: utf-8 -*-
+
 from PIL import Image
 import numpy
 
-# naredimo random array vrednosti od 0-255 dimenzij 200x200x3 in spremenimo tip na uint8
-# (to je en byte, toliko rabimo za opis barve, od 0 - 255)
-array = numpy.uint8(numpy.random.randint(0,255,size=(800,800,3)))
+# Naredimo random array vrednosti od 0-255 dimenzij 800x800x3 in spremenimo tip
+# na uint8 (to je en byte, toliko rabimo za opis barve, od 0 - 255).
+# S tem torej naredimo sliko velikosti 800x800 pikslov, ki ima RGB barve. Če bi
+# želeli RGBA (še transparentnost) bi naredili array velikosti 800x800x4
+array = numpy.uint8(numpy.random.randint(0, 255, size=(800, 800, 3)))
+
 # array laho naredimo tudi tako, da spremenimo pythonov list
-# a = numpy.array([[[0,0,255],[0,0,255]],[[0,0,255],[0,0,255]]], dtype=numpy.uint8)
+# a = numpy.array(
+#     [[[0, 0, 255], [0, 0, 255]], [[0, 0, 255], [0, 0, 255]]],
+#     dtype=numpy.uint8
+# )
 
 #naredimo sliko in jo pokažemo
-img = Image.fromarray(array,'RGB')
-img.show() 
+img = Image.fromarray(array, 'RGB')
+img.show()
