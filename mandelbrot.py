926a4c78b1c4c9055e85a7847aee0062979cc43f
Jure Slak <jure.slak@gmail.com>
2014-02-20 14:03:30 +0100

    Added support for iterations parameter from outside.

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,10 +1,9 @@
 from PIL import Image
 from color import *
 
-iterations = 50
 color = Color(255, 128, 0)
 
-def is_inside(x, y):
+def is_inside(x, y, iterations):
     c = complex(x, y)
     z = 0
     for i in range(iterations):
@@ -14,7 +13,7 @@ def is_inside(x, y):
             break
     return i/iterations
 
-def make_fractal(ulx, uly, drx, dry, img_width):
+def make_fractal(ulx, uly, drx, dry, img_width, iterations):
     img_height = int((uly-dry)/(drx-ulx) * img_width)
     print (img_width,img_height)
     im = Image.new("RGB",(img_width,img_height),tuple(BLACK))
@@ -24,10 +23,10 @@ def make_fractal(ulx, uly, drx, dry, img_width):
         for py in range(img_height):
             x = ulx + px/img_width * (drx-ulx)
             y = dry + py/img_height * (uly-dry)
-            scalar = is_inside(x,y)
+            scalar = is_inside(x,y,iterations)
             pix[px,py] = color.multuple(scalar)
 
     return im
 
 def default_settings():
-    return (-2.2,1.2,0.7,-1.2,500)
+    return (-2.2,1.2,0.7,-1.2,500,50)
